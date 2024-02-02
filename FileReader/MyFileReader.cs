68159cb71a607f41ed5f6e5fbbeb7e7924b03fcc
KovalYevhenii <kovalyevhenii@gmail.com>
2024-02-02 13:16:53 +0100

    Filereader class added

---
+++ FileReader/MyFileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    internal class MyFileReader
+    {
+        private int _spaceCount;
+        public int SpaceCount
+        {
+            get => _spaceCount;
+        }
+        public Task ReadAllFileNamespaces(string path)
+        {
+            return Task.Run(() =>
+            {
+                using var reader = new StreamReader(path);
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                     _spaceCount += line.Count(char.IsWhiteSpace);
+                }
+            });
+        }
+    }
+}
---
+++ FileReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace FileReader
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            MyFileReader read = new();
+            Stopwatch stopwatch = new();
+            var filePath = Directory.GetFiles(@"Your_Path");
+
+            stopwatch.Start();
+            var tasks = filePath.Select(async filePath =>
+            {
+                await read.ReadAllFileNamespaces(filePath);
+                return read.SpaceCount;
+            });
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Total number of whitespaces:{read.SpaceCount}, execution Time: {stopwatch.ElapsedMilliseconds} ms.");
+        }
+    }
+}
